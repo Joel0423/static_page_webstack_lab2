f7b8248c6c33f78f3658e660b0a342c9e2f95b92
Joel Abhishek <joelabhishek123@gmail.com>
2024-07-28 23:50:06 +0530

    1

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function scrollL() {
+    const container = document.getElementById('scrollable');
+    container.scrollBy(-200, 0);
+}
+
+function scrollRight() {
+    const container = document.getElementById('scrollable');
+    container.scrollBy(200, 0,);
+}
